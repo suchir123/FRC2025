f959048a6754e66bfd4fdb5d13f21952b6b5fba2
tankmaster48 <craigshuaogu@gmail.com>
2025-02-04 17:45:34 -0800

    climber with no features

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -1,10 +1,14 @@
 package frc.robot.subsystems;
 
 
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.config.SparkBaseConfig;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import edu.wpi.first.math.geometry.Rotation2d;
 
 import frc.robot.Constants;
 import frc.robot.util.ThroughboreEncoder;
@@ -21,6 +25,30 @@ public class ClimberSubsystem extends SubsystemBase{
         throughboreEncoder = new ThroughboreEncoder(Constants.PortConstants.CLIMBER_ABS_ENCODER_ID, 0, false);
         pidController = climbMotor.getClosedLoopController();
 
-        
+        SparkMaxConfig climbMotorConfig = new SparkMaxConfig();
+
+        climbMotorConfig
+                .inverted(false)
+                .idleMode(SparkBaseConfig.IdleMode.kBrake)
+                .voltageCompensation(12);
+        climbMotorConfig.closedLoop
+                .pidf(1, 0, 0, 0)
+                .outputRange(-0.6, 0.6);
+
+        climbMotor.configure(climbMotorConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
+        throughboreEncoder.name = "climber";
+    }
+
+    @Override
+    public void periodic() {
+        throughboreEncoder.periodic();
+    }
+
+    public void setRawSpeed(double speed) {
+        climbMotor.set(speed);
+    }
+
+    public Rotation2d getThroughboreEncoderDistance() {
+        return throughboreEncoder.getTotalDistance();
     }
 }
