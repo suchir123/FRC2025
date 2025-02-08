0edd77c85cf71e597d1dbffb0b1b4a55182336bf
siiverfish <ivan.g.zimmerman@gmail.com>
2025-02-08 14:56:22 -0800

    fix algae intake subsystem

--- src/main/java/frc/robot/subsystems/AlgaeIntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/AlgaeIntakeSubsystem.java
@@ -9,11 +9,13 @@ import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import edu.wpi.first.math.geometry.Rotation2d;
-
+import edu.wpi.first.wpilibj.Servo;
 import frc.robot.Constants;
+import frc.robot.Constants.PortConstants;
 import frc.robot.util.ThroughboreEncoder;
 
 public class AlgaeIntakeSubsystem extends SubsystemBase{ // This subsystem needing much more feature only has 1 neo currently
+    // public class 
 
     private final SparkMax algaeIntakeMotor;
     private final Servo    algaeLeftServo;
@@ -21,8 +23,9 @@ public class AlgaeIntakeSubsystem extends SubsystemBase{ // This subsystem needi
 
     public AlgaeIntakeSubsystem(){
         algaeIntakeMotor = new SparkMax(Constants.PortConstants.CAN.ALGAE_MOTOR_ID, MotorType.kBrushless);
-        algaeLeftServo = new Servo(PortConstants.ALGAE_LEFT_SERVO_PORT)
-        algaeRightServo = new Servo(PortConstants.ALGAE_RIGHT_SERVO_PORT)
+        // leaving these undefined seems better than adding them with bad values
+        algaeLeftServo = new Servo(PortConstants.ALGAE_LEFT_SERVO_PORT);
+        algaeRightServo = new Servo(PortConstants.ALGAE_RIGHT_SERVO_PORT);
         
         SparkMaxConfig algaeMotorConfig = new SparkMaxConfig();
 
